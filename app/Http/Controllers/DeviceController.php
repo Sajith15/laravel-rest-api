9d0dc207b4016d6085372ecc7582a5e4f3d1e2f6
Sajith1509 <kpsajith2000@gmail.com>
2022-07-21 00:53:19 +0530

    added optional params condition for list route and function

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -7,8 +7,8 @@ use App\Models\Device;
 
 class DeviceController extends Controller
 {
-    function list()
+    function list($id=null)
     {
-        return Device::all();
+        return $id?Device::find($id):Device::all();
     }
 }
