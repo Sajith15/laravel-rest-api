2b15a6eeda5315b66e1a071b803b37bf2ba3f66c
Sajith1509 <kpsajith2000@gmail.com>
2022-07-21 00:45:26 +0530

    db connection and select all from db added

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\simpleAPI;
+use App\Http\Controllers\DeviceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +15,8 @@ use App\Http\Controllers\simpleAPI;
 |
 */
 
-Route::get('data',[simpleAPI::class, 'getData']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('list',[DeviceController::class, 'list']);
